24a7079bcf75fcb3cc8c51c2026fc651b7a94722
Yash Hokte <hokteyash4@gmail.com>
2025-11-02 16:33:21 +0530

    Practice Spring security concepts

--- src/main/java/com/hokte/SpringSecurityPractice/filters/JwtFilter.java
+++ src/main/java/com/hokte/SpringSecurityPractice/filters/JwtFilter.java
@@ -28,6 +28,7 @@ public class JwtFilter extends OncePerRequestFilter {
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        // checking the auth
         String authHeader = request.getHeader("Authorization");
         String token = null;
         String username = null;
